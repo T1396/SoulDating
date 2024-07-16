92ae3a3ed0897d7548400817b64bc91ce30cbd13
Philipp T <ptirop@icloud.com>
2024-07-16 13:15:06 +0200

    updated video player

--- SoulDating/Views/Auth/FullScreenVideoView.swift
+++ SoulDating/Views/Auth/FullScreenVideoView.swift
@@ -25,6 +25,8 @@ struct FullScreenVideoView: UIViewRepresentable {
 class PlayerUIView: UIView {
     private var player: AVPlayer?
     private var playerLayer: AVPlayerLayer?
+    private var playerStatusObserver: NSKeyValueObservation? // Holds the observation object
+
 
     init(frame: CGRect, url: URL) {
         super.init(frame: frame)
@@ -32,30 +34,30 @@ class PlayerUIView: UIView {
     }
 
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
     private func setupPlayer(url: URL) {
         player = AVPlayer(url: url)
-        player?.addObserver(self, forKeyPath: "status", options: [.old, .new], context: nil)
         playerLayer = AVPlayerLayer(player: player)
         playerLayer?.videoGravity = .resizeAspectFill // fill whole screen
         playerLayer?.frame = self.bounds
+
         if let layer = playerLayer {
             self.layer.addSublayer(layer)
+        }
+
+        playerStatusObserver = player?.observe(\.status, options: [.old, .new]) { [weak self] _, _ in
+            if let status = self?.player?.status {
+                print("AVPlayer status: \(status.rawValue)")
+            }
         }
         player?.play()
         player?.isMuted = true
         print("should play video")
         NotificationCenter.default.addObserver(self, selector: #selector(restartVideo), name: .AVPlayerItemDidPlayToEndTime, object: player?.currentItem)
-    }
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == "status" {
-            if let status = player?.status {
-                print("AVPlayer status: \(status.rawValue)")
-            }
-        }
     }
 
     override func layoutSubviews() {
@@ -70,5 +72,9 @@ class PlayerUIView: UIView {
     @objc private func restartVideo() {
         player?.seek(to: .zero)
         player?.play()
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 }
